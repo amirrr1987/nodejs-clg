a501545110d51b364d9af34aa1e8a5a7c7eb12d6
amirrr1987 <maghami.a1987@gmail.com>
2022-04-11 12:08:47 -0400

    feat: use chalk module
    - change methods width chalk
    - use path module to get js path

--- logger.js
+++ logger.js
@@ -1,36 +1,22 @@
 import { fileURLToPath, URL } from "url";
+import path from 'path'
+import chalk from 'chalk';
 
-export default function logger(data) {
-  console.debug(
-    "--------------------- Start log ------------------------------"
-  );
-  // console.log("%cName is : " + `%cname   `, "color: blue;", "color: red;");
-  console.log(
-    "%cFile is: " + `%c${fileURLToPath(new URL(import.meta.url))}`,
-    "color: blue;",
-    "color: red;"
-  );
-  console.log(
-    "%cType is : " + `%c${typeof data}   `,
-    "color: blue;",
-    "color: red;"
-  );
-  if (typeof data == "object") {
-    console.log(
-      "%cis Array: " + `%c${Array.isArray(data)} `,
-      "color: blue;",
-      "color: red;"
-    );
+const currentPath = fileURLToPath(new URL(import.meta.url))
+const fileName = path.basename(currentPath)
+
+const logger = (data) => {
+  console.log(chalk.blue("--------------------- Start log ------------------------------"));
+  console.log(chalk.blue(`- File name is: `), chalk.red(fileName))
+  console.log(chalk.blue(`- File location is: `), chalk.red(currentPath))
+  console.log(chalk.blue(`- Type is : `), chalk.red(typeof data))
+  console.log(chalk.blue(`- Is this an Array?`), chalk.red(Array.isArray(data) ? true : false))
+  if (Array.isArray(data) === true) {
+    console.log(chalk.blue(`- Array Length: `), chalk.red(data.length))
   }
-  if (Array.isArray(data) == true) {
-    console.log(
-      "%cArray Length: " + `%c${data.length} `,
-      "color: blue;",
-      "color: red;"
-    );
-  }
-  console.log("%cdata return  : ", "color: blue;");
+  console.log(chalk.blue(`- Data return  : `))
   console.dir(data);
-
-  console.debug("--------------------- End log ------------------------------");
+  console.log(chalk.blue("---------------------  End log  ------------------------------"));
 }
+
+export default logger
